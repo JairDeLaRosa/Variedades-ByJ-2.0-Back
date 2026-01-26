fb8f57d1d2e6b6eb14632f34bfdc5e57069f36df
Jair De La Rosa <yairdlrv@gmail.com>
2026-01-25 21:03:16 -0500

    first deploy

--- src/ventas/ventas.service.ts
+++ src/ventas/ventas.service.ts
@@ -12,41 +12,49 @@ export class VentasService {
 
   async create(createVentaDto: CreateVentaDto): Promise<ResponseCreateRPT> {
     try {
-      const fecha = new Date(createVentaDto.fecha);
+      const fecha = new Date(`${createVentaDto.fecha}T00:00:00-05:00`); // Colombia
+
       if (isNaN(fecha.getTime())) {
         throw new HttpException(
           "La fecha proporcionada no es válida.",
           HttpStatus.BAD_REQUEST,
         );
       }
-      const fechaDia = fecha.toISOString().split("T")[0];
+
+      const fechaDia = fecha.toLocaleDateString("sv-SE", {
+        timeZone: "America/Bogota",
+      });
 
       const existingVenta = await this.ventaModel.findOne({ fechaDia });
+
       let resDb;
       let msg;
+
       if (existingVenta) {
-        // Update existing
         const updateData = {
           ...createVentaDto,
           fecha,
           fechaDia,
         };
+
         resDb = await this.ventaModel.findByIdAndUpdate(
           existingVenta._id,
           updateData,
           { new: true },
         );
+
         msg = "Registro actualizado.";
       } else {
-        // Create new
         const venta = new this.ventaModel({
           ...createVentaDto,
           fecha,
           fechaDia,
         });
+
         resDb = await venta.save();
         msg = "Registro creado.";
       }
+
       return {
         data: resDb,
         ok: true,
